9d02b878644d0cb37ae4b0b3d7803740077abb7b
ednaldo <ednaldo.contato1@gmail.com>
2023-02-11 22:21:50 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopmangoApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmangoApplication.class, args);
+	}
+
+}
