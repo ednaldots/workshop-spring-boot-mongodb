626773358ce71f817243ca6e0289e7b33c995ac2
ednaldo <ednaldo.contato1@gmail.com>
2024-03-28 16:37:57 -0300

    Inserção de usuário com POST

--- src/main/java/com/nelioalves/worshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/worshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.worshopmongo.domain.User;
+import com.nelioalves.worshopmongo.dto.UserDTO;
 import com.nelioalves.worshopmongo.repository.UserRepository;
 import com.nelioalves.worshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -24,4 +25,21 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto){
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
+
+
+
+
+
+
+
+
+
