76cb1848196724d48f17440ff48719d5cffc57a8
ednaldo <ednaldo.contato1@gmail.com>
2024-03-28 11:44:43 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/nelioalves/worshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.nelioalves.worshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.nelioalves.worshopmongo.domain.User;
+import com.nelioalves.worshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
