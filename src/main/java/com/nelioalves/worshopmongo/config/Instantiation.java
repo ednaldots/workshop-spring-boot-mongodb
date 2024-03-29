c4b2e3f4c726041682e65b015e6426bc234c6284
ednaldo <ednaldo.contato1@gmail.com>
2024-03-29 09:03:07 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nelioalves/worshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/worshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.nelioalves.worshopmongo.domain.Post;
 import com.nelioalves.worshopmongo.domain.User;
 import com.nelioalves.worshopmongo.dto.AuthorDTO;
+import com.nelioalves.worshopmongo.dto.CommentDTO;
 import com.nelioalves.worshopmongo.repository.PostRepository;
 import com.nelioalves.worshopmongo.repository.UserRepository;
 
@@ -19,42 +20,40 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO (maria));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
-		
+
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/20218"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/20218"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/20218"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
 		maria.getPost().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
 
 }
-
-
-
-
-
-
-
-
-
--- src/main/java/com/nelioalves/worshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/worshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.nelioalves.worshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.nelioalves.worshopmongo.dto.AuthorDTO;
+import com.nelioalves.worshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
@@ -68,6 +73,16 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
